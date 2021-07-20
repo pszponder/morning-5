6ef3ad3197cfa3ff34c91dbf092d11583c8a0498
Piotr Szponder <14116413+pszponder@users.noreply.github.com>
2021-07-20 13:38:14 -0600

    update files to pull quote from api onpage reload

--- js/quote.js
+++ js/quote.js
@@ -1,23 +1,34 @@
-// https://theysaidso.com/api/#qod
+// const getQuote = () => {
+//   // e.preventDefault();
+//   console.log("running function");
 
-// function get_quote_of_the_day() {
-//   var xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//       // Access the result here
-//       alert(this.responseText);
-//     }
-//   };
-//   xhttp.open("GET", "https://quotes.rest/qod?category=inspire", true);
-//   xhttp.setRequestHeader("Content-type", "application/json");
-//   xhttp.setRequestHeader("X-Theysaidso-Api-Secret", "YOUR API HERE");
-//   xhttp.send();
-// }
+//   // Call the fetch function on the randomuser.me api
+//   let userPromise = fetch("https://randomuser.me/api/");
+//   console.log("Logging promise from fetch function");
+//   console.log(userPromise);
 
-// get_quote_of_the_day();
-// https://quotes.rest/qod
-// https://randomuser.me/api/
+//   // Evaluate the promise returned from the fetch function
+//   userPromise
+//     .then((response) => {
+//       console.log("Response, waiting to parse...", response);
+
+//       // Parse the Response object from the promise using the .json() method
+//       return response.json();
+//     })
+//     // Extract the data from the promise returned by response.json()
+//     .then((resData) => {
+//       console.log("Data Parsed:...", resData);
+//       // console.log(resData.contents.quotes[0].quote);
+//       console.log(resData.results[0].name.first);
+//       console.log(resData.results[0].name.last);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+
 const getQuote = () => {
+  // e.preventDefault();
   console.log("running function");
 
   // Call the fetch function on the randomuser.me api
@@ -36,21 +47,14 @@ const getQuote = () => {
     // Extract the data from the promise returned by response.json()
     .then((resData) => {
       console.log("Data Parsed:...", resData);
-      // console.log(resData.contents.quotes[0].quote);
-      console.log(resData.results[0].name.first);
-      console.log(resData.results[0].name.last);
+      console.log(resData.contents.quotes[0].quote);
+      document.getElementById("qod").innerText =
+        resData.contents.quotes[0].quote;
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
-const sayHello = () => {
-  console.log("I was clicked");
-};
-
-// const quoteGenerator = document.querySelector(".button");
-// quoteGenerator.addEventListener("click", getQuote);
-document.getElementById("retrieveQuote").addEventListener("click", sayHello);
-// getQuote();
-// document.getElementById();
+// document.getElementById("retrieveQuote").addEventListener("click", getQuote);
+getQuote();
